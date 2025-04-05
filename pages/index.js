f20a26236c4c398efe9d2e2ba51b1869060599fe
Chandan kumar <chandankrbit3@gmail.com>
2025-04-05 20:12:29 +0530

    gtag added

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 
 import ChartVisualizer from "@/components/HomePage";
-
+import Head from 'next/head';
 // import ExcelLikeTableWithChartOptions from "@/components/InputSheets";
 
 
 export default function Home() {
   return (
     
-    <div>
+    <>
+      <Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-BJPK6YK3MV"></script>
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-BJPK6YK3MV');
+        `}
+      </script>
+    </Head>
       {/* <ExcelLikeTableWithChartOptions/> */}
       <ChartVisualizer/>
   
-    </div>
+    </>
   );
 }
